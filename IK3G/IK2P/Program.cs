6c56b2224fe6957bfc3a31182f6aa239f40b7cca
ArIkAr20 <107494416+ArIkAr20@users.noreply.github.com>
2022-06-15 00:51:00 +0700

    Add files via upload

---
+++ IK3G/IK2P/Program.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] mas = new int[n];
+Random rand = new Random();
+int index = 0;
+for (int i = 0; i < n; i++)
+{
+    mas[i] = rand.Next(0, 20);
+    Console.WriteLine(mas[i]);
+}
+int max = 0;
+for (int i = 0; i < n - 2; i++)
+{
+    
+    int s = mas[i] + mas[i + 1] + mas[i + 2];
+    if (s > max)
+    {
+        index = i;
+        max = s;
+    }
+}
+Console.WriteLine("Числа образующие максимальную сумму");
+Console.WriteLine(mas[index]);
+Console.WriteLine(mas[index + 1]);
+Console.WriteLine(mas[index + 2]);
