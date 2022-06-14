b61e4da68e6fa8c6346835a9bd88f3cdee21037b
ArIkAr20 <107494416+ArIkAr20@users.noreply.github.com>
2022-06-15 00:52:22 +0700

    Add files via upload

---
+++ IK4G/IK3P/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = int.Parse(Console.ReadLine());
+int j = int.Parse(Console.ReadLine());
+int[] mas = new int[n];
+for (int i = 0; i < n; i++)
+{
+    mas[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine(mas[j]);
---
+++ IK6G/IK6P/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 1; i <= num; i = i + 2)
+{
+    if (num % i == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
